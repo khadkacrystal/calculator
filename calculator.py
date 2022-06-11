4979a3a28664ee7bbe4bef4cf3ecbbda098cf07d
khadkacrystal <63010983+khadkacrystal@users.noreply.github.com>
2022-06-11 18:54:08 +0545

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,8 +1,6 @@
 from tkinter import *
 root=Tk()
-root.title("Calculator")
-root.resizable(0,0)
-root.configure(background='#f2f2f2')
+
 
 e=Entry(root,width=30,borderwidth=5)
 e.grid(row=0,column=0,columnspan=4,padx=10,pady=10)
@@ -27,17 +25,6 @@ def button_subtract():
     math="subtraction"
     f_num=int(first_number)
     e.delete(0,END)
-def button_equal():
-    second_number=e.get()
-    e.delete(0,END)
-    if math=="addition":
-        e.insert(0,f_num+int(second_number))
-    if math=="subtraction":
-        e.insert(0,f_num-int(second_number))
-    if math=="multiplication":
-        e.insert(0,f_num*int(second_number))
-    if math=="division":
-        e.insert(0,f_num/int(second_number))
 def button_multiply():
     first_number=e.get()
     global f_num
@@ -52,6 +39,19 @@ def button_divide():
     math="division"
     f_num=int(first_number)
     e.delete(0,END)
+def button_equal():
+    second_number=e.get()
+    e.delete(0,END)
+    if math=="addition":
+        e.insert(0,f_num+int(second_number))
+    if math=="subtraction":
+        e.insert(0,f_num-int(second_number))
+    if math=="multiplication":
+        e.insert(0,f_num*int(second_number))
+    if math=="division":
+        e.insert(0,f_num/int(second_number))
+
+
 
 Button_1=Button(root,text="1",padx=40,pady=20,command=lambda:button_click(1))
 Button_2=Button(root,text="2",padx=40,pady=20,command=lambda:button_click(2))
